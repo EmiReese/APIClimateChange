e46faadce4495c34b540557261cdef92326efe55
Emily Reese <emilyreese.ga@gmail.com>
2022-11-28 11:07:18 -0500

    I found out where the bug is in the program. I need help resolving it. I commented the area with the bug.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+
+
 const PORT = process.env.PORT || 8000
 const express = require('express')
 const axios = require('axios')
+
 const cheerio = require('cheerio')
 const app = express()
 
@@ -74,33 +77,34 @@ const newspapers = [
 
 const articles = []
 
-// newspapers.forEach(newspaper => {
-//     axios.get(newspaper.address)
-//         .then(response => {
-//             const html = response.data
-//             const $ = cheerio.load(html)
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    //there is a bug right here
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
 
-//             $('a:contains("climate")', html).each(function () {
-//                 const title = $(this).text()
-//                 const url = $(this).attr('href')
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
 
-//                 articles.push({
-//                     title,
-//                     url: newspaper.base + url,
-//                     source: newspaper.name
-//                 })
-//             })
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
 
-//         })
-// })
+        })
+})
 
 app.get('/', (req, res) => {
     res.json('Welcome to my Climate Change News API')
 })
 
-// app.get('/news', (req, res) => {
-//     res.json(articles)
-// })
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
 
 app.get('/news/:newspaperId', (req, res) => {
     const newspaperId = req.params.newspaperId
